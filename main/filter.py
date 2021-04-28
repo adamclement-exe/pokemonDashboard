3122105d58497c9fe5d1c5fe2887be5305c1cf8a
Cameron Phillips <cameron.phillips.clp@gmail.com>
2021-04-28 15:25:45 +0100

    [addition] comments

--- main/filter.py
+++ main/filter.py
@@ -6,8 +6,8 @@ except ImportError:
 root = Tk()
 root.title('Pokemon Index Finder')
 
-HEIGHT = 342
-WIDTH = 205
+HEIGHT = 342 # 642
+WIDTH = 205 # 205
 
 root.minsize(WIDTH, HEIGHT)
 root.maxsize(WIDTH, HEIGHT)
@@ -29,19 +29,24 @@ InnerFrame.place(relx=0.5, rely=0.09,
                  relwidth=0.90, relheight=0.75,
                  anchor='n')
 
-InnerFrame1 = Frame(root,
+ButtonFrame = Frame(root,
                    bg='#dfe2ea')
 
-InnerFrame1.place(relx=0.5, rely=0.87,
+ButtonFrame.place(relx=0.5, rely=0.87,
                  relwidth=1, relheight=0.12,
                  anchor='n')
 
 poke_name = Entry(InnerFrame,
+
                   bg='#dfe2ea', fg='black',
+
                   font=70, borderwidth=5)
+
 poke_name.place(relx=0.25, rely=0.105,
                 relwidth=0.5, relheight=0.09)
+
 poke_name.insert(0, f'Name')
+
 
 Pokemon_Browser = Label(root,
 
@@ -52,14 +57,14 @@ Pokemon_Browser = Label(root,
 Pokemon_Browser.place(relx=0.17, rely=0,
                       relwidth=0.66, relheight=0.09)
 
-Pokemon_Browser["text"] = f'Pokemon Browser'
+Pokemon_Browser["text"] = f'Pokemon Browser' # title
 
 
-help_button = Button(InnerFrame1,
+help_button = Button(ButtonFrame,
                      bg='#9c9fa5',
                      fg='#dfe2ea',
                      font=('times', 11, 'bold'), borderwidth=4,
-                     command=lambda: help())
+                     command=lambda: help()) # runs when help button is clicked
 
 help_button.place(relx=0.25, rely=0.3,
                    relheight=0.08, relwidth=0.50)
