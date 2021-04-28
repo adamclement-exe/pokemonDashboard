182357cc3d54314af59b9699b1465615e6a920f9
Cameron Phillips <cameron.phillips.clp@gmail.com>
2021-04-28 12:22:57 +0100

    [addition] added new gui folder location and moved menu into that

---
+++ gui/__init__.py
@@ -0,0 +1,56 @@
+try:
+    from Tkinter import *
+except ImportError:
+    from tkinter import *
+root = Tk()
+HEIGHT = 500
+WIDTH = 600
+
+canvas = Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+frame1 = Frame(root,
+               bg='#2E4053')
+
+frame1.place(relx=0.5, rely=0.14,
+             relwidth=0.90, relheight=0.75,
+             anchor='n')
+
+text1 = Label(root,
+              bg='#2E4053',
+              fg='white',
+              font=('times', 11, 'bold'))
+
+text1.place(relx=0.06, rely=0.21,
+            relwidth=0.15, relheight=0.09)
+
+text1["text"] = 'DEVS:'
+
+text2 = Label(root,
+              bg='#2E4053',
+              fg='white',
+              font=('times', 11, 'bold'))
+
+text2.place(relx=0.06, rely=0.3,
+            relwidth=0.18, relheight=0.3)
+
+text2[
+    "text"] = 'Madmegsox1\n' + 'FSNCryo\n' + 'Basker 12\n' + 'Hiddenmaask\n' + 'PointlessQuack\n' + 'DraconicDroid\n' + 'Squidnugi\n'
+
+start_button = Button(frame1,
+                      bg='white',
+                      font=('times', 11, 'bold'), borderwidth=4,
+
+                      command=lambda: on_button_push())  # on_button_push() Runs When a BUTTON is Pushed
+
+start_button.place(relx=0.25, rely=0.765,
+                   relheight=0.08, relwidth=0.50)
+
+start_button["text"] = f'START'
+
+
+def on_button_push():  # Run Main GUI
+    return
+
+
+root.mainloop()
