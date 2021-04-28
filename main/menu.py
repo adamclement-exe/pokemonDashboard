8838f82cf839e1da58e96af74d5e4380e4dc5638
Cameron Phillips <cameron.phillips.clp@gmail.com>
2021-04-28 10:55:04 +0100

    [addition] menu.py is more Readable

--- main/menu.py
+++ main/menu.py
@@ -8,22 +8,33 @@ WIDTH = 600
 canvas = Canvas(root,height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-frame1 = Frame(root, bg='#2E4053')
+frame1 = Frame(root,
+               bg='#2E4053')
+
 frame1.place(relx=0.5, rely=0.14,
-             relwidth=0.90,relheight=0.75,
+             relwidth=0.90, relheight=0.75,
              anchor='n')
 
 
 
-text1 = Label(root,bg='#2E4053', fg='white',font=('times', 11, 'bold'))
-text1.place(relx=0.06,rely=0.21,
+text1 = Label(root,
+              bg='#2E4053',
+              fg='white',
+              font=('times', 11, 'bold'))
+
+text1.place(relx=0.06, rely=0.21,
             relwidth=0.15, relheight=0.09)
 
 text1["text"] = f'DEVS:'
 
 
-text2 = Label(root,bg='#2E4053', fg='white',font=('times', 11, 'bold'))
-text2.place(relx=0.06,rely=0.365,relwidth=0.18, relheight=0.09)
+text2 = Label(root,
+              bg='#2E4053',
+              fg='white',
+              font=('times', 11, 'bold'))
+
+text2.place(relx=0.06, rely=0.3,
+            relwidth=0.18, relheight=0.3)
 
 text2["text"] = f'Madmegsox1\n' \
                 f'FSNCryo\n' \
@@ -34,8 +45,11 @@ text2["text"] = f'Madmegsox1\n' \
                 f'Squidnugi\n'
 
 
-start_button = Button(frame1, bg='white',font=('times', 11, 'bold'), borderwidth=4,
-                 command=lambda: on_button_push()) # on_button_push() Runs When a BUTTON is Pushed
+start_button = Button(frame1,
+                      bg='white',
+                      font=('times', 11, 'bold'), borderwidth=4,
+
+                      command=lambda: on_button_push()) # on_button_push() Runs When a BUTTON is Pushed
 
 
 start_button.place(relx=0.25, rely=0.765,
