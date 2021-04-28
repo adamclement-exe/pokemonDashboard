d8fa44a4ce6cc6b3e983dd14437828b5b96c591f
JLey21 <83337978+JLey21@users.noreply.github.com>
2021-04-28 13:56:45 +0100

    [Addition] Author Update

--- main/__init__.py
+++ main/__init__.py
@@ -2,7 +2,7 @@
 :author Madmegsox1
 :author FSNCryo
 :author Basker 12
-:author Jaime
+:author JLey21
 """
 
 import util
@@ -12,7 +12,7 @@ import util
 
 class run():
     def __init__(self):
-        print("Project by: Madmegsox1, FSNCryo, HiddenMask, Basker12, PointlessQuack, DraconicDroid, Squidnugi")
+        print("Project by: Madmegsox1, FSNCryo, HiddenMask, Basker12, Jley21, DraconicDroid, Squidnugi")
         print(util.csv_loader("Pokemon.csv").get_pokemon_by_category("type1", "Rock"))
 
 
