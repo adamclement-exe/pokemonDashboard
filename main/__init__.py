158c044651f57a76a89945b701e808b3e1ef71c1
Madmegsox1 <68086870+Madmegsox1@users.noreply.github.com>
2021-04-28 10:46:56 +0100

    [Addition] Added Util Class

--- main/__init__.py
+++ main/__init__.py
@@ -3,3 +3,9 @@
 """
 
 # This is the base of the project
+
+class run():
+    def __init__(self):
+        print("Project by: Madmegsox1, FSNCryo, HiddenMask, Basker12, PointlessQuack, DraconicDroid, Squidnugi")
+
+run()
