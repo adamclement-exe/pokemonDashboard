a59df6cdf2d1b27ad655fc7a5a68295f480bffc5
Basker12 <71788109+Basker12@users.noreply.github.com>
2021-04-28 12:07:11 +0100

    [Addition] Changed around the colours for the background to fit the set theme we picked and i have also changed the title name to 'Pokemon Index Finder'.

--- main/menu.py
+++ main/menu.py
@@ -1,7 +1,10 @@
 try: from Tkinter import *
 except ImportError: from tkinter import *
+import tkinter as tk
 
 root = Tk()
+root.title('Pokemon Index Finder')
+
 HEIGHT = 500
 WIDTH = 600
 
@@ -9,24 +12,22 @@ canvas = Canvas(root,height=HEIGHT, width=WIDTH)
 canvas.pack()
 
 frame1 = Frame(root,
-               bg='#2E4053')
+               bg='#5778BB')
 
 frame1.place(relx=0.5, rely=0.14,
              relwidth=0.90, relheight=0.75,
              anchor='n')
 
 
-
 text1 = Label(root,
               bg='#2E4053',
-              fg='white',
+              fg='#DFE2EA',
               font=('times', 11, 'bold'))
 
 text1.place(relx=0.06, rely=0.21,
             relwidth=0.15, relheight=0.09)
 
 text1["text"] = 'DEVS:'
-
 
 text2 = Label(root,
               bg='#2E4053',
@@ -36,11 +37,12 @@ text2 = Label(root,
 text2.place(relx=0.06, rely=0.3,
             relwidth=0.18, relheight=0.3)
 
-text2["text"] = 'Madmegsox1\n'+ 'FSNCryo\n'+ 'Basker 12\n' + 'Hiddenmaask\n' + 'PointlessQuack\n' + 'DraconicDroid\n' + 'Squidnugi\n'
+text2["text"] = ('Madmegsox1\n' + 'FSNCryo\n' + 'Basker 12\n' + 'Hiddenmaask\n' + 'PointlessQuack\n' + 'DraconicDroid\n' + 'Squidnugi\n')
 
 
 start_button = Button(frame1,
-                      bg='white',
+                      bg='#9C9FA5',
+                      fg='#DFE2EA',
                       font=('times', 11, 'bold'), borderwidth=4,
 
                       command=lambda: on_button_push()) # on_button_push() Runs When a BUTTON is Pushed
@@ -52,8 +54,8 @@ start_button.place(relx=0.25, rely=0.765,
 start_button["text"] = f'START'
 
 def on_button_push(): # Run Main GUI
-    return
+    print("Test")
     
-
+#HEX Colours: #9C9FA5 - Grey | #5778BB - Blue | #DFE2EA - white
 
 root.mainloop()
