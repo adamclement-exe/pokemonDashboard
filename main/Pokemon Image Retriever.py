a4ffae622cea0d598940be7443ae0850f3c311bb
JLey21 <83337978+JLey21@users.noreply.github.com>
2021-04-30 10:00:17 +0100

    [Addition] Code Update

--- main/Pokemon Image Retriever.py
+++ main/Pokemon Image Retriever.py
@@ -18,7 +18,7 @@ from selenium import webdriver
 # This is the path I use
 # DRIVER_PATH = '.../Desktop/Scraping/chromedriver 2'
 # Put the path for your ChromeDriver here
-DRIVER_PATH = 'M:\pk\pokemonDashboard\main/chromedriver'
+DRIVER_PATH = ''
 wd = webdriver.Chrome(executable_path=DRIVER_PATH)
 wd.get('https://google.com')
 search_box = wd.find_element_by_css_selector('input.gLFyf')
