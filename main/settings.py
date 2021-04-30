0f6f2abc786d7434920084a8fb80384e5c517358
Basker12 <71788109+Basker12@users.noreply.github.com>
2021-04-30 12:30:33 +0100

    [Addition] Added Settings file and going to start working on it.

--- main/settings.py
+++ main/settings.py
@@ -1,1 +1,20 @@
+try:
+    from Tkinter import *
+except ImportError:
+    from tkinter import *
 
+root = Tk()
+root.title('Settings')
+
+HEIGHT = 475
+WIDTH = 475
+root.geometry(f'{WIDTH}x{HEIGHT}')
+
+root.minsize(WIDTH, HEIGHT)
+root.maxsize(WIDTH, HEIGHT)
+
+iconFile = PhotoImage(file='formating/ball.png')
+root.iconphoto(False, iconFile)
+
+
+root.mainloop()
