7144e17a4ccfd85a61b58afde61dc845b342aecf
Itzkacper12 <71788109+Itzkacper12@users.noreply.github.com>
2021-05-03 17:57:16 +0100

    [Addition] Now you're able to write into the entry boxes and when you click the 'Create Pokemon' button it will print it out.

--- main/addnewpokemon.py
+++ main/addnewpokemon.py
@@ -54,11 +54,37 @@ newType2.place(relx=0.25, rely=0.315,
 
 newType2.insert(0, f'Your Pokemons second type')
 
+createButton = Button(root, text='CREATE POKEMON', font=('times', 12, 'bold'), borderwidth='4',
+                     bg='#9C9FA5',
+                     fg='#5778BB',
+                     width='15',
+                      command=lambda: createdPokemon())
+
+createButton.place(relx=0.25, rely=0.420,
+                   relheight=0.09, relwidth=0.5)
+
 def clearText(e): # This definition clears the entry box text, instead having to do it manually
+
     if newPokeName.get() == 'Your new Pokemons name' or newType1.get() == 'Your Pokemons first type' or newType2.get() == 'Your Pokemons second type':
         newPokeName.delete(0, END)
         newType1.delete(0, END)
         newType2.delete(0, END)
+
+def createdPokemon():
+
+    pokename = newPokeName.get()
+    if pokename == pokename:
+        name = pokename.title()
+
+    poketype1 = newType1.get()
+    if poketype1 == poketype1:
+        type1 = poketype1.title()
+
+    poketype2 = newType2.get()
+    if poketype2 == poketype2:
+        type2 = poketype2.title()
+
+    print(f"This is your new pokemon, his name is {name}, it's first type is {type1}, and it's second type is {type2}")
 
 newPokeName.bind("<Button-1>", clearText) # clearText runs the definition
 newType1.bind("<Button-1>", clearText)
