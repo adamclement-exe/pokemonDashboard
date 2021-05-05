9650449cd4685892d76b1d48f906c4e13f2f55b6
JLey21 <83337978+JLey21@users.noreply.github.com>
2021-05-05 09:19:08 +0100

    [Addition] Converted jpg to png

--- main/Pokemon Pictures/JPEG_to_PNG_copy.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image  # Python Image Library - Image Processing
-import glob
-
-for file in glob.glob("*.png"):
- im = Image.open(file)
- rgb_im = im.convert('RGB')
- rgb_im.save(file.replace("png", "jpg"), quality=95)
--- main/Pokemon Pictures/JPG_remover_copy.py
+++
@@ -1,6 +0,0 @@
-import os
-from PIL import Image  # Python Image Library - Image Processing
-import glob
-
-for file in glob.glob("*.jpg"):
-    os.remove(file)
