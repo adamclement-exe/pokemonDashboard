872f922dc4cc71ffca510b663b225c34b5c896c3
Basker12 <71788109+Basker12@users.noreply.github.com>
2021-05-05 10:37:37 +0100

    [Addition] Added the rest of the Entry boxes with the required elements to go into the CSV file, and also did that each individual entry box will only clear it's text, before it would clear all the entry boxes text with it.

--- main/addnewpokemon.py
+++ main/addnewpokemon.py
@@ -45,7 +45,7 @@ newType1 = Entry(root, font=('times', 8, 'bold'), borderwidth='4',
 newType1.place(relx=0.60, rely=0.105,
                relheight=0.09, relwidth=0.38)
 
-newType1.insert(0, f'Your Pokemons first type')
+newType1.insert(0, f'Pokemons first type')
 
 newType2 = Entry(root, font=('times', 8, 'bold'), borderwidth='4',
                  bg='#9C9FA5',
@@ -54,7 +54,7 @@ newType2 = Entry(root, font=('times', 8, 'bold'), borderwidth='4',
 newType2.place(relx=0.02, rely=0.210,
                relheight=0.09, relwidth=0.38)
 
-newType2.insert(0, f'Your Pokemons second type')
+newType2.insert(0, f'Pokemons second type')
 
 newGen = Entry(root, font=('times', 8, 'bold'), borderwidth='4',
                bg='#9C9FA5',
@@ -69,10 +69,66 @@ newLegendary = Entry(root, font=('times', 8, 'bold'), borderwidth='4',
                      bg='#9C9FA5',
                      fg='#5778BB')
 
-newLegendary.place(relx=0.30, rely=0.315,
+newLegendary.place(relx=0.30, rely=0.750,
                    relheight=0.09, relwidth=0.38)
 
 newLegendary.insert(0, f'Is the Pokemon a legendary?')
+
+
+newAttack = Entry(root, font=('times', 8, 'bold'), borderwidth='4',
+                  bg='#9C9FA5',
+                  fg='#5778BB')
+
+newAttack.insert(0, f'Pokemons attack stat')
+
+newAttack.place(relx=0.02, rely=0.315,
+                relheight=0.09, relwidth=0.38)
+
+newHP = Entry(root, font=('times', 8, 'bold'), borderwidth='4',
+              bg='#9C9FA5',
+              fg='#5778BB')
+
+newHP.insert(0, f'Pokemons HP')
+
+newHP.place(relx=0.60, rely=0.315,
+            relheight=0.09, relwidth=0.38)
+
+newDefence = Entry(root, font=('times', 8, 'bold'), borderwidth='4',
+                   bg='#9C9FA5',
+                   fg='#5778BB')
+
+newDefence.insert(0, f'Pokemons defence stat')
+
+newDefence.place(relx=0.02, rely=0.420,
+                 relheight=0.09, relwidth=0.38)
+
+newSpecialAttack = Entry(root, font=('times', 8, 'bold'), borderwidth='4',
+                       bg='#9C9FA5',
+                       fg='#5778BB')
+
+newSpecialAttack.insert(0, f'Pokemons special attack')
+
+newSpecialAttack.place(relx=0.60, rely=0.420,
+                     relheight=0.09, relwidth=0.38)
+
+newSpecialDefence = Entry(root, font=('times', 8, 'bold'), borderwidth='4',
+                       bg='#9C9FA5',
+                       fg='#5778BB')
+
+newSpecialDefence.insert(0, f'Pokemons special defence')
+
+newSpecialDefence.place(relx=0.60, rely=0.525,
+                     relheight=0.09, relwidth=0.38)
+
+newSpeed = Entry(root, font=('times', 8, 'bold'), borderwidth='4',
+                 bg='#9C9FA5',
+                 fg='#5778BB')
+
+newSpeed.insert(0, f'Pokemons Speed')
+
+newSpeed.place(relx=0.02, rely=0.525,
+               relheight=0.09, relwidth=0.38)
+
 
 createButton = Button(root, text='CREATE POKEMON', font=('times', 12, 'bold'), borderwidth='4',
                      bg='#9C9FA5',
@@ -80,19 +136,52 @@ createButton = Button(root, text='CREATE POKEMON', font=('times', 12, 'bold'), b
                      width='15',
                       command=lambda: createdPokemon())
 
-createButton.place(relx=0.25, rely=0.420,
+createButton.place(relx=0.25, rely=0.800,
                    relheight=0.09, relwidth=0.5)
 
 def clearText(e): # This definition clears the entry box text, instead having to do it manually
-
-    if newPokeName.get() == 'Your new Pokemons name' or newType1.get() == 'Your Pokemons first type' or newType2.get() == 'Your Pokemons second type' \
-    or newGen == 'Pokemons Generation' or newLegendary == 'Is the Pokemon a legendary':
-
+    if newPokeName.get() == 'Your new Pokemons name':
         newPokeName.delete(0, END)
+
+def clearTextNT1(e):
+    if newType1.get() == 'Pokemons first type':
         newType1.delete(0, END)
+
+def clearTextNT2(e):
+    if newType2.get() == 'Pokemons second type':
         newType2.delete(0, END)
+
+def clearTextG(e):
+    if newGen.get() == 'Pokemons Generation':
         newGen.delete(0, END)
+
+def clearTextLEG(e):
+    if newLegendary.get() == 'Is the Pokemon a legendary?':
         newLegendary.delete(0, END)
+
+def clearTextSPEED(e):
+    if newSpeed.get() == 'Pokemons Speed':
+        newSpeed.delete(0, END)
+
+def clearTextAT(e):
+    if newAttack.get() == 'Pokemons attack stat':
+        newAttack.delete(0, END)
+
+def clearTextDEF(e):
+    if newDefence.get() == 'Pokemons defence stat':
+        newDefence.delete(0, END)
+
+def clearTextSAT(e):
+    if newSpecialAttack.get() == 'Pokemons special attack':
+        newSpecialAttack.delete(0, END)
+
+def clearTextSDEF(e):
+    if newSpecialDefence.get() == 'Pokemons special defence':
+        newSpecialDefence.delete(0, END)
+
+def clearTextHP(e):
+    if newHP.get() == 'Pokemons HP':
+        newHP.delete(0, END)
 
 def createdPokemon():
 
@@ -118,20 +207,27 @@ def createdPokemon():
 
     list = [name, poketype1, poketype2, pokegen, pokelegnd]
 
-    with open('Pokemon.csv', 'a') as pk:  # Opens Pokemon.csv and the writes into the csv file a new pokemon
+    with open('temp.csv', 'a') as pk:  # Opens Pokemon.csv and the writes into the csv file a new pokemon
         Pokemon = writer(pk)
         Pokemon.writerow(list)
         pk.close()
 
-'''
+
     print(f"This is your new pokemon, his name is {name}, it's first type is {type1}, and it's second type is {type2}, "
           f"It's generation is {gen}, Legendary: {legendary}")
-'''
+
 newPokeName.bind('<Button-1>', clearText) # clearText runs the definition
-newType1.bind('<Button-1>', clearText)
-newType2.bind('<Button-1>', clearText)
-newGen.bind('<Button-1>', clearText)
-newLegendary.bind('<Button-1>', clearText)
+newType1.bind('<Button-1>', clearTextNT1)
+newType2.bind('<Button-1>', clearTextNT2)
+newGen.bind('<Button-1>', clearTextG)
+newLegendary.bind('<Button-1>', clearTextLEG)
+newSpeed.bind('<Button-1>', clearTextSPEED)
+newAttack.bind('<Button-1>', clearTextAT)
+newDefence.bind('<Button-1>', clearTextDEF)
+newSpecialAttack.bind('<Button-1>', clearTextSAT)
+newSpecialDefence.bind('<Button-1>', clearTextSDEF)
+newHP.bind('<Button-1>', clearTextHP)
+
 
 # HEX Colours: #9C9FA5 - Grey | #5778BB - Blue | #DFE2EA - white
 root.mainloop()
