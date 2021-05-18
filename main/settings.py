b983a8bdd8ce104cc2bc0d28ec00c0310791c47e
DraconicDroid <74269451+DraconicDroid@users.noreply.github.com>
2021-05-18 15:55:22 +0100

    # Can change font of text in button in settings.py but need help for more than that

--- main/settings.py
+++ main/settings.py
@@ -19,7 +19,7 @@ root.geometry(f'{WIDTH}x{HEIGHT}')
 root.minsize(WIDTH, HEIGHT)
 root.maxsize(WIDTH, HEIGHT)
 
-iconFile = PhotoImage(file='formating/ball.png') # Icon image
+iconFile = PhotoImage(file='formating/ball.png')  # Icon image
 root.iconphoto(False, iconFile)
 
 backgroundImage = PhotoImage(file='formating/settingsbackgroundimage.png')
@@ -27,8 +27,8 @@ backgroundImage = PhotoImage(file='formating/settingsbackgroundimage.png')
 Canvas = Canvas(root, width=WIDTH, height=HEIGHT, highlightbackground='#9C9FA5', highlightthickness='4')
 Canvas.pack(fill='both', expand='True')
 Canvas.create_image(0, 0, image=backgroundImage,
-                    anchor='nw') # Created an canvas so that text and buttons are able to show the background
-#home
+                    anchor='nw')  # Created an canvas so that text and buttons are able to show the background
+# home
 home_button = Button(root,
                      bg='#9C9FA5',
                      fg='#5778BB',
@@ -40,45 +40,54 @@ home_button.place(relx=0.1, rely=0.900,
 
 home_button["text"] = f'Home'
 
-#help
+# help
 help_button = Button(root, text='Help', font=('times', 12, 'bold'), borderwidth='4',
                      bg='#9C9FA5',
                      fg='#5778BB',
-                      command=lambda:helpsection())
+                     command=lambda: helpsection())
 
 help_button.place(relx=0.55, rely=0.900,
-                   relheight=0.075, relwidth=0.35)
+                  relheight=0.075, relwidth=0.35)
 
 OPTIONS = [
-    "Font",
-    "Font",
-    "Font",
-    "Font",
-    "Fonts"
-]  #list with fonts
+    'Times',
+    'Comic sans MS',
+    'Helvetica',
+    'Garamond'
+]  # list with fonts
 
 variable = StringVar(root)
 variable.set(OPTIONS[0])
 
-font_drop = OptionMenu(root, variable, *OPTIONS)  # drop down menu for fonts
+
+def callback(selection):
+    font_drop.config(font=(selection, 12, 'bold'))
+    help_button.config(font=(selection, 12, 'bold'))
+    home_button.config(font=(selection, 12, 'bold'))
+
+
+font_drop = OptionMenu(root, variable, *OPTIONS, command=callback)  # drop down menu for fonts
 font_drop.config(bg='#9C9FA5', fg='#5778BB', font=('times', 12, 'bold'))
 font_drop.place(relx=0.33, rely=0.750,
-                   relheight=0.075, relwidth=0.35) # places drop down menu
+                relheight=0.075, relwidth=0.35)  # places drop down menu
 
 titleImage = PhotoImage(file='formating/settingsLogo.png')
 titleImage = titleImage.zoom(2)
 titleImage = titleImage.subsample(3)
 Canvas.create_image(250, 94, image=titleImage)
 
+
 def mainmenu():
     pygame.mixer.music.stop()
     root.destroy()
     os.system('python menu.py')
 
+
 def helpsection():
     pygame.mixer.music.stop()
     root.destroy()
     os.system('python Help_Menu.py')
+
 
 # HEX Colours: #9C9FA5 - Grey | #5778BB - Blue | #DFE2EA - white
 
